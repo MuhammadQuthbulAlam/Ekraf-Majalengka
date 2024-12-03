98f97e19695b8216ee743fb4961a74f7b9877029
Mochan Endeavour <mochanendeavour@gmail.com>
2024-12-03 20:58:17 +0700

    Update

--- app/component/Komite.jsx
+++ app/component/Komite.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import mcc from "../../public/Assets/images/MCC3.jpg";
 import {
   FaGamepad,
   FaHandScissors,
@@ -180,60 +181,72 @@ const Komite = () => {
             Komite Ekonomi{" "}
             <span className="font-bold text-sky-400">Kreatif</span>
           </h1>
-          <div className="grid md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-3">
-            <div className="box pt-5">
-              <h1
-                className="text-left lg:text-2xl/tight text-2xl font-medium mb-3 ml-10"
-                id="Tentang-Ekraf"
-              >
-                Tentang Komite
-                <span className="font-bold text-sky-400"> Ekraf</span>
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-3">
+            <div className="box">
+              <div className="box pt-5">
+                <h1
+                  className="text-left lg:text-2xl/tight text-2xl font-medium mb-3 ml-10"
+                  id="Tentang-Ekraf"
+                >
+                  Tentang Komite
+                  <span className="font-bold text-sky-400"> Ekraf</span>
+                </h1>
+                <div className="text-left mx-10 bg-white border-2 rounded-lg w-auto h-auto">
+                  <div className="text-base px-2">
+                    Komite pengembangan ekonomi kreatif majalengka adalah
+                    lembaga mitra pemerintahan dalam penyusunan kegiatan
+                    pengembangan, mengarahkan serta mengawal pelaksanaan program
+                    aksi pengembangan ekonomi kreatif di kabupaten majalengka
+                    sesuai dengan keputusan bupati majalengka nomor
+                    eka.02.01/kep.513-bapedalitbang/2023.
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <Image
+                  src={mcc}
+                  alt=""
+                  className="lg:w-[675px] h-[400px] md:m-10 mx-auto rounded-lg"
+                />
+              </div>
+            </div>
+            {/*Visi & Misi*/}
+            <div className="box pt-5" id="visimisi">
+              <h1 className="text-left ml-10 lg:text-2xl/tight text-2xl font-medium mb-2">
+                Visi & Misi{" "}
+                <span className="font-bold text-sky-400">Ekraf</span>
               </h1>
-              <div className="text-left mx-10 bg-white border-2 rounded-lg w-auto h-auto">
-                <div className="text-base px-2">
-                  Komite pengembangan ekonomi kreatif majalengka adalah lembaga
-                  mitra pemerintahan dalam penyusunan kegiatan pengembangan,
-                  mengarahkan serta mengawal pelaksanaan program aksi
-                  pengembangan ekonomi kreatif di kabupaten majalengka sesuai
-                  dengan keputusan bupati majalengka nomor
-                  eka.02.01/kep.513-bapedalitbang/2023.
+              <h3 className="pt-1 ml-10 text-2xl font-medium text-sky-400">
+                Visi
+              </h3>
+              <div className="text-left mx-10 bg-white border-2 rounded-lg w-auto h-auto items-center">
+                <div className="text-base px-2 text-wrap">
+                  Terwujudnya ekonomi kreatif sebagai sektor potensial unggulan
+                  yang mendukung pertumbuhan ekonomi di kabupaten majalengka.
                 </div>
               </div>
-            </div>
-          </div>
-          {/*Visi & Misi*/}
-          <div className="box pt-5" id="visimisi">
-            <h1 className="text-left ml-10 lg:text-2xl/tight text-2xl font-medium mb-7">
-              Visi & Misi <span className="font-bold text-sky-400">Ekraf</span>
-            </h1>
-            <h3 className="pt-1 ml-10 text-2xl font-medium text-sky-400">
-              Visi
-            </h3>
-            <div className="text-left mx-10 bg-white border-2 rounded-lg w-auto h-auto items-center">
-              <div className="text-base px-2 text-wrap">
-                Terwujudnya ekonomi kreatif sebagai sektor potensial unggulan
-                yang mendukung pertumbuhan ekonomi di kabupaten majalengka.
-              </div>
-            </div>
-            <h3 className="pt-5 ml-10 text-2xl font-medium text-sky-400">
-              Misi
-            </h3>
-            <div className="text-left mx-10 bg-white border-2 rounded-lg w-auto h-auto items-center">
-              <div className="text-base px-2 text-wrap">
-                <span className="font-bold">1.</span> Mengoptimalisasi aktivasi
-                gedung majalengka creative center sebagai ruang edukasi kreasi
-                dan ekspresi bagi para pelaku ekonomi kreatif di kabupaten
-                majalengka
-                <div>
-                  <span className="font-bold">2.</span> Mendorong pertumbuhan
-                  dan perkembangan ekosistem ekonomi kreatif di kabupaten
-                  majalengka
-                </div>
-                <div>
-                  <span className="font-bold">3.</span> Mendorong pemerintah kab
-                  majalengka dalam menerbitkan peraturan daerah tentang ekonomi
-                  kreatif sebagai payung hukum bagi tumbuh kembangangnya
-                  ekosistem ekonomi kreatif di kabupaten majalengka
+              <h3 className="pt-5 ml-10 text-2xl font-medium text-sky-400">
+                Misi
+              </h3>
+              <div className="text-left mx-10 bg-white border-2 rounded-lg w-auto h-auto items-center">
+                <div className="text-base px-2 text-wrap">
+                  <span className="font-bold">1.</span> Mengoptimalisasi
+                  aktivasi gedung majalengka creative center sebagai ruang
+                  edukasi kreasi dan ekspresi bagi para pelaku ekonomi kreatif
+                  di kabupaten majalengka
+                  <div>
+                    <span className="font-bold">2.</span> Mendorong pertumbuhan
+                    dan perkembangan ekosistem ekonomi kreatif di kabupaten
+                    majalengka
+                  </div>
+                  <div>
+                    <span className="font-bold">3.</span> Mendorong pemerintah
+                    kab majalengka dalam menerbitkan peraturan daerah tentang
+                    ekonomi kreatif sebagai payung hukum bagi tumbuh
+                    kembangangnya ekosistem ekonomi kreatif di kabupaten
+                    majalengka
+                  </div>
                 </div>
               </div>
             </div>
