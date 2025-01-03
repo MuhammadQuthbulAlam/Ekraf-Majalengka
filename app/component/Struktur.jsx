c1021a2806ab7341da04fc263e4a662ad949e9b2
Mochan Endeavour <mochanendeavour@gmail.com>
2025-01-03 14:32:54 +0700

    Update

--- app/component/Struktur.jsx
+++ app/component/Struktur.jsx
@@ -6,7 +6,7 @@ const Struktur = () => {
     <>
       <h1 className="pt-32 text-center lg:text-2xl/tight text-2xl font-medium mb-5">
         Struktur Organisasi
-        <span className="font-bold text-sky-400"> Ekraf</span>
+        <span className="font-bold text-sky-400"> MCC</span>
       </h1>
       <p className="text-center">
         Struktur Organisasi Majalengka Creative Center
